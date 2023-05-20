96327db718208f115037bd8fdc89a3c3db5772e2
spu-lvky <1352283598@qq.com>
2023-05-20 17:03:00 +0800

    数独终盘生成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include "sudoku_generate.h"
 using namespace std;
 
-int main() {
+extern int sudokuGenerate(int num, const char* filename);
+
+int main(int argc,char argv[]) {
+	const char* a = "result.txt";
+	sudokuGenerate(2, a);
 	return 0;
 }
---
+++ sudoku_generate.h
@@ -0,0 +1,7 @@
+#include <cstdlib>
+#include <time.h>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int sudokuGenerate(int num, const char* filename);
